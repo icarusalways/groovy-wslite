372353ba22d1cbd89b22dee41e9b76af72b0de50
U-BLUES\y12098 <y12098@HNW67592.blues.HealthNow.local>
2014-12-11 10:24:26 -0500

    added ability to send json payload over rest

--- src/main/groovy/wslite/rest/ContentBuilder.groovy
+++ src/main/groovy/wslite/rest/ContentBuilder.groovy
@@ -14,6 +14,7 @@
  */
 package wslite.rest
 
+import groovy.json.JsonOutput   //Added by HealthNow to support creation of JSON payload from a closure.    
 import groovy.xml.*
 import wslite.http.*
 import wslite.json.*
@@ -104,6 +105,12 @@ class ContentBuilder {
     void json(List content) {
         dataContentType = ContentType.JSON
         data = new JSONArray(content).toString()?.getBytes(getCharset())
+    }
+
+    //Added by HealthNow to support creation of JSON payload from a closure.    
+    void json(Closure content) {
+        dataContentType = ContentType.JSON
+        data = JsonOutput.toJson(content).toString()?.getBytes(getCharset())
     }
 
     String getCharset() {
